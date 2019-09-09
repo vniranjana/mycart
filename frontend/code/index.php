f4b3c72e1f784081b0a3665af2d147f70f3da3b4
Ubuntu <ubuntu@ip-172-31-23-75.ec2.internal>
2019-09-09 12:57:36 +0000

    mycart

--- frontend/code/index.php
+++ frontend/code/index.php
@@ -12,7 +12,10 @@ curl_close($conn);
 return $response;
 }
 $backendlb = getenv("BACKENDLB");
-$backendurl="http://$backendlb/backend.php";
-$backendoutput=httpPost($backendurl);
-echo $backendoutput;
+$baseurl="http://$backendlb/bootstrap.php";
+$response=httpPost($baseurl);
+echo $response;
+$testurl="http://$backendlb/getusers.php";
+$candllabassoc_id=httpPost($testurl);
+echo $candllabassoc_id;
 ?>
